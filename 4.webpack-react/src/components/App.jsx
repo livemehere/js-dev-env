ed2fb2c39f129ff087269fd00c0c4e527736cf24
livemehere <rhdxoals@gmail.com>
2023-06-02 05:40:20 +0900

    react 테스트

--- 4.webpack-react/src/components/App.jsx
+++ 4.webpack-react/src/components/App.jsx
@@ -1,9 +1,14 @@
 import data from '@public/data.json'
+import {useState} from "react";
 
 export default function App(){
     console.log(data)
+    const [count, setCount] = useState(0);
+
     return <div>
-        <h1>피카츄</h1>
+        <h1>피카츄12</h1>
+        <div>{count}</div>
+        <button onClick={()=> setCount(prev=> prev+1)}>UPUP</button>
         <img src='/pica.png' alt=""/>
     </div>
 }
